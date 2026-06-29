4a9291856d250ec2c1c354f6ca07cff9b0405ac1
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:28:09 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Membrane.Stream.Mixfile do
     [
       {:membrane_core, "~> 1.0"},
       {:membrane_file_plugin, "~> 0.16.0", only: :test},
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
